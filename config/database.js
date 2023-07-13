bdf82ab89df8e1694e69dcf80af2d70f4744f4c2
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2023-07-12 21:50:40 -0500

    Conexion

--- config/database.js
+++ config/database.js
@@ -1,13 +1,16 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "mysql",
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'anime'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env(
+        "DATABASE_HOST",
+        "animecol.c0ugrfgwmkbn.us-east-1.rds.amazonaws.com"
+      ),
+      port: env.int("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "anime"),
+      user: env("DATABASE_USERNAME", "awsanimecol"),
+      password: env("DATABASE_PASSWORD", "nik.2000"),
+      ssl: env.bool("DATABASE_SSL", true),
     },
   },
 });
