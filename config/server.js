4c5d725eed05919c10e9e0d35030fd1da924c778
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2023-07-16 18:39:56 -0500

    Configurando

--- config/server.js
+++ config/server.js
@@ -1,8 +1,13 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  url: "https://anime-back.up.railway.app/api/", // I was referring to this one
+  proxy: true, // This makes Strapi (and therefore the Koa instance) aware of the proxy.
+  admin: {
+    url: "https://anime-back.up.railway.app/admin/",
+    autoOpen: false,
   },
 });
