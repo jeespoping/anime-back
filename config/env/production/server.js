ef7e73ff200854fb772be3617534d7f25edf6e47
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2023-07-17 10:54:08 -0500

    publicasda

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -4,6 +4,9 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
   url: "https://anime-back-production.up.railway.app/", // I was referring to this one
   proxy: true, // This makes Strapi (and therefore the Koa instance) aware of the proxy.
   admin: {
