4b6148dec4ace98362782255968785f91dd92ad0
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2023-07-17 09:57:11 -0500

    deploy

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,12 +2,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
+      host: env("DATABASE_HOST"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "anime"),
-      user: env("DATABASE_USERNAME", "root"),
-      password: env("DATABASE_PASSWORD", ""),
-      ssl: env.bool("DATABASE_SSL", true),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: { rejectUnauthorized: false },
     },
+    debug: false,
   },
 });
