715601d7768afeb27dd7bc4766dec6d7bc44c1aa
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2023-07-29 21:46:28 -0500

    Agregando cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -27,4 +27,13 @@ module.exports = ({ env }) => [
       },
     },
   },
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["animecol.com"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+      keepHeaderOnError: true,
+    },
+  },
 ];
