21a4ec36295a83087fd7c39d628f3c22aedf530f
Jesus-Lopez-F <jesus.lopez@lasamericas.com.co>
2023-10-06 12:47:08 -0500

    Montando nuevo mysql

--- config/database.js
+++ config/database.js
@@ -1,13 +1,12 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
+    client: "mysql2",
     connection: {
       host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 3306),
       database: env("DATABASE_NAME", "anime"),
       user: env("DATABASE_USERNAME", "root"),
       password: env("DATABASE_PASSWORD", ""),
-      ssl: env.bool("DATABASE_SSL", true),
     },
   },
 });
